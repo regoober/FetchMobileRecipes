ad46519f62e87ff2c9386018719488848e036831
Brian Goo <regoob@gmail.com>
2024-10-04 23:31:13 -0700

    Initial Commit

---
+++ FetchMobileRecipes/FetchMobileRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMobileRecipesApp.swift
+//  FetchMobileRecipes
+//
+//  Created by Brian Goo on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchMobileRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
